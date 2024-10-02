5b305710259273f5df3fc28f73529984ef62de43
Tkachenko Pavel <Tkachenko.PI@dns-shop.ru>
2024-10-02 10:10:00 +1000

    FizzBuzz

--- LessonOne/Program.cs
+++ LessonOne/Program.cs
@@ -10,6 +10,7 @@
             {
                 if (taskNumber == 1) RemainderDivisionethod();
                 else if (taskNumber == 2) CopyArray();
+                else if (taskNumber == 3) FizzBuzz();
 
                 Console.WriteLine("===================================================================");
                 taskNumber = GetStartNumber();
@@ -101,37 +102,62 @@
 
         private static void FizzBuzz()
         {
-            
+            //Задача состоит в том, чтобы вывести все числа от 1 до 100.Но если число является кратным 3,
+            //вместо него следует вывести слово «Fizz». Если число кратно 5, выводим «Buzz». Если число
+            //кратно как 3, так и 5(например, 15 или 30), то выводим «FizzBuzz». Например, «1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz...»
+           
+            Console.WriteLine("ДЗ: FizzBuzz");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0) Console.Write("FizzBuzz");
+                else if (i % 3 == 0) Console.Write("Fizz");
+                else if (i % 5 == 0) Console.Write("Buzz");
+                else Console.Write(i);
+
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
         }
 
         private static void MinimumAndAverageValues()
         {
 
         }
+        
         private static void MonthsListing()
         {
 
         }
+        
         private static void FlipArray()
         {
 
         }
+        
         private static void TwoCars()
         {
 
         }
+        
         private static void Salary()
         {
 
         }
+        
         private static void Deposits()
         {
 
         }
+        
         private static void MonthDays()
         {
 
         }
+        
         private static void TicTacToe()
         {
 
